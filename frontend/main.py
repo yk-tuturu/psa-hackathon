ef5433b0e54d3887f53ac7706bfaa9114299f232
yk-tuturu <waikei.koh@gmail.com>
2025-10-19 10:43:50 +0800

    Fix frontend display bugs

--- frontend/main.py
+++ frontend/main.py
@@ -2,20 +2,16 @@ import streamlit as st
 import time
 import requests
 
-BACKEND_URL = ""
+BACKEND_URL = "http://127.0.0.1:8000"
 
 st.set_page_config(page_title="Chatbot", page_icon="💬", layout="centered")
 
 st.title("💬 PSA Network Insights Dashboard")
 
-report_res = requests.get(f"{BACKEND_URL}/report").json()
-token_res = requests.get(f"{BACKEND_URL}/embed-token").json()
-
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
 
-# Display chat history with custom icons
 for msg in st.session_state["messages"]:
     with st.chat_message(msg["role"]):
         st.markdown(msg["content"])
@@ -27,9 +23,14 @@ if prompt := st.chat_input("Type your message..."):
 
     with st.chat_message("user"):
         st.markdown(prompt)
+    
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        message_placeholder.markdown("...")
 
         payload = {
-            "history": messages,  # entire conversation so far
+            "history": st.session_state["messages"],  # entire conversation so far
             "message": prompt     # new user message
         }
 
